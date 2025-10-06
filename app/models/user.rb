a60ebb24d56e60b1320d959db89b6a6266628ace
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-10-06 05:04:43 +0000

    validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :pet_type, presence: true, length: { maximum: 50 }
+  validates :pet_name, presence: true, length: { maximum: 30 }
+  validates :bio, presence: true, length: { maximum: 200 }
+  
 end
