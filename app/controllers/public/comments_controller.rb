5b78627df30191c4e3c80df28e867a090ad328f2
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-10-14 11:01:39 +0000

    comment

---
+++ app/controllers/public/comments_controller.rb
@@ -0,0 +1,34 @@
+class Public::CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+
+    if @post.user_id == current_user.id
+      redirect_to post_path(@post), alert: "自分の投稿にはコメントできません"
+      return
+    end
+
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to post_path(@post), notice: "コメントを投稿しました"
+    else
+      redirect_to post_path(@post), alert: "コメントを投稿できませんでした"
+    end
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @post = @comment.post
+    @comment.destroy
+    redirect_to post_path(@post), notice: "コメントを削除しました"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_06_052306) do
+ActiveRecord::Schema.define(version: 2025_10_14_102444) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2025_10_06_052306) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -50,5 +60,7 @@ ActiveRecord::Schema.define(version: 2025_10_06_052306) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
