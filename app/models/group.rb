aa99a6d58ec11c5a6539a4b8f8279590ba9a8f87
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-10-18 09:33:20 +0000

    message1

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :group_members, dependent: :destroy
   has_many :members, -> { where(group_members: { status: :approved }) }, through: :group_members, source: :user
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 end
