d0fa671c75eb3ddae06dc1e3095c83c8d5f607aa
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-10-20 10:35:58 +0000

    first

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "../stylesheets/application.scss"
 
 Rails.start()
 Turbolinks.start()
