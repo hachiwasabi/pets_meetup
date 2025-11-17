ee57e096834dd50d8c7d1cb2542962d6d0eea424
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-11-17 09:37:31 +0000

    layout

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -14,13 +14,13 @@ class Public::RelationshipsController < ApplicationController
   end
 
   def create
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    redirect_to request.referer
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
   end
 
 end
