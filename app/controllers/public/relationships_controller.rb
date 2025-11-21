fed0e19269a33a877c7b81b7445b3e555a345d24
hachiwasabi <224736665+hachiwasabi@users.noreply.github.com>
2025-11-21 02:58:14 +0000

    readable_code

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,26 +1,34 @@
 class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:create, :destroy]
+  before_action :set_follow, only: [:follows, :followers]
 
   def follows
-    @user = User.find(params[:id])
     @users = @user.following_users.with_attached_profile_image.page(params[:page]).per(12)
     render :follows
   end
 
   def followers
-    @user = User.find(params[:id])
     @users = @user.follower_users.with_attached_profile_image.page(params[:page]).per(12)
     render :followers
   end
 
   def create
-    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
   end
 
   def destroy
-    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
   end
 
+  private
+
+  def set_follow
+    @user = User.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+  
 end
